564314286dc46bafbb23b3eaac3c8fc236aa204c
aa485 <aa485@duke.edu>
2020-10-06 17:05:17 -0500

    added standard error

--- scheduler.py
+++ scheduler.py
@@ -3,21 +3,22 @@ import sys
 
 ## error handling for cases where no file is presented or file is the wrong file type
 if len(sys.argv) < 2:
-	print("Please specify a JSON file argument.")
+	# go through and change these to standard error
+	print("Error: No argument provided.", file=sys.stderr)
 	quit()
 
 else:
 	json_file = sys.argv[1]
 
 if json_file.find(".") == -1:
-	print("Please specify a JSON file argument.")
+	print("Error: No argument provided.", file=sys.stderr)
 	quit()
 
 else:
 	file_extension = json_file.split(".")[1]
 
 if file_extension != "json":
-	print("Please specify a JSON file argument.")
+	print("Error: No argument provided.", file=sys.stderr)
 	quit()
 
 with open(json_file) as f:
