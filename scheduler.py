196ad3645bc60bbd37ab1e5f7906ad2ac0de3122
aa485 <aa485@duke.edu>
2020-10-06 16:59:29 -0500

    added error handling for file inputs

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,24 @@
 import json
 import sys
 
-json_file = sys.argv[1]
+## error handling for cases where no file is presented or file is the wrong file type
+if len(sys.argv) < 2:
+	print("Please specify a JSON file argument.")
+	quit()
+
+else:
+	json_file = sys.argv[1]
+
+if json_file.find(".") == -1:
+	print("Please specify a JSON file argument.")
+	quit()
+
+else:
+	file_extension = json_file.split(".")[1]
+
+if file_extension != "json":
+	print("Please specify a JSON file argument.")
+	quit()
 
 with open(json_file) as f:
 	classes = json.load(f)
